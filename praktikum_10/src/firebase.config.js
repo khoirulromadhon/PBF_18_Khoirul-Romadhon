842b42d40767733b0806121090fb3c34e52c73d2
khoirulromadhon <khoirulromadhon1112@gmail.com>
2022-04-25 14:14:57 +0700

    Praktikum-10

---
+++ praktikum_10/src/firebase.config.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+export default{
+    apiKey: "AIzaSyAZO8ZsSkPjZ-yHlptVac5O2h2Ir0s_2cA",
+    authDomain: "antrian-ea5c4.firebaseapp.com",
+    databaseURL: "https://antrian-ea5c4-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "antrian-ea5c4",
+    storageBucket: "antrian-ea5c4.appspot.com",
+    messagingSenderId: "124622402581",
+    appId: "1:124622402581:web:419e72289203412368ca1c",
+    measurementId: "G-PFMXR3VDJH"
+};
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
